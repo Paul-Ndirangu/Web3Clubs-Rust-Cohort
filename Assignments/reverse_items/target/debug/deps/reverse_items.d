d812b599663b037cfc89537504ba9916c94bf58e
paul-ndirangu <paulmwaura254@gmail.com>
2024-05-16 19:16:03 +0300

    task 2 initial project commit

---
+++ Assignments/reverse_items/target/debug/deps/reverse_items.d
@@ -0,0 +1,5 @@
+D:\codelab\rust\Web3Clubs-Rust-Cohort\Assignments\reverse_items\target\debug\deps\reverse_items.exe: src\main.rs
+
+D:\codelab\rust\Web3Clubs-Rust-Cohort\Assignments\reverse_items\target\debug\deps\reverse_items.d: src\main.rs
+
+src\main.rs:
---
+++ Assignments/reverse_items/target/debug/reverse_items.d
@@ -0,0 +1,1 @@
+D:\codelab\rust\Web3Clubs-Rust-Cohort\Assignments\reverse_items\target\debug\reverse_items.exe: D:\codelab\rust\Web3Clubs-Rust-Cohort\Assignments\reverse_items\src\main.rs
